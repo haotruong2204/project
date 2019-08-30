cf7e3d8da0ead897778ad1cfe06d96c6c5418ecc
hao truong <haotv03@gmail.com>
2019-08-30 08:33:28 +0700

    robocop

--- test/controllers/admin/dashboards_controller_test.rb
+++ test/controllers/admin/dashboards_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Admin::DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get admin_dashboards_index_url
     assert_response :success
   end
-
 end
