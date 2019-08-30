e978b522a9b7301b7e0546a00ec87782bd02be45
hao truong <haotv03@gmail.com>
2019-08-30 10:33:34 +0700

    Login

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,5 +1,4 @@
-# frozen_string_literal: true
-
-class Admin::DashboardsController < ApplicationController
-  def index; end
+class Admin::DashboardsController < Admin::BaseController
+  def index
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+  def after_sign_out_path_for resource_or_scope
+    return new_admin_admin_session_path if resource_or_scope == :admin_admin
+
+    super
+  end
+
+  def after_sign_in_path_for resource_or_scope
+    return admin_root_path if resource_or_scope.is_a?(Admin)
+
+    super
+  end
 end
